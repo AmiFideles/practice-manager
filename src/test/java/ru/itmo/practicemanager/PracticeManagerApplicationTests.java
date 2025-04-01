87df14e6e77b6ba8de9637783f50a0ef6853b1d1
AmiFideles <251645@niuitmo.ru>
2025-04-01 20:15:02 +0300

    init project

---
+++ src/test/java/ru/itmo/practicemanager/PracticeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itmo.practicemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
