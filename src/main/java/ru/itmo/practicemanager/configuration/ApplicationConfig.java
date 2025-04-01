b3b68c70ea1a50a9c3a1046e43edc80ab45c3da4
AmiFideles <251645@niuitmo.ru>
2025-04-01 20:16:43 +0300

    add open api

---
+++ src/main/java/ru/itmo/practicemanager/configuration/ApplicationConfig.java
@@ -0,0 +1,14 @@
+package ru.itmo.practicemanager.configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Practice Manager API",
+                version = "1.0",
+                description = "API для автоматизации бизнес-процесса по распределению студентов на практику"))
+public class ApplicationConfig {
+}
