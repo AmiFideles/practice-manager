1baa41b9a21dab7a99c45be860cb4c27a7e13535
nurgun <makarov@mail.ru>
2025-04-17 03:49:16 +0300

    fixes

--- bot/src/main/java/org/example/studentdistributionbot/configuration/WebClientConfiguration.java
+++ bot/src/main/java/org/example/studentdistributionbot/configuration/WebClientConfiguration.java
@@ -4,14 +4,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
 
 @Configuration
 public class WebClientConfiguration {
 
     @Bean
     public WebClient webClient() {
+        HttpClient client = HttpClient.create()
+                .responseTimeout(Duration.ofSeconds(30));
         return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(client))
                 .baseUrl("http://localhost:8082")
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
